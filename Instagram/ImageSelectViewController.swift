4cb52c73eeb941074df062614ab692c5b8366139
amy <358amy@gmail.com>
2021-04-27 15:11:41 +0900

    Podfile、Podfile.lock、.gitignoreの作成

---
+++ Instagram/ImageSelectViewController.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by Ami Hirahara on 2021/04/27.
+//
+
+import Foundation
+
+class ImageSelectViewController: UIViewController {
+
+
+
+}
+
---
+++ Instagram/PostViewController.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Instagram
+//
+//  Created by Ami Hirahara on 2021/04/27.
+//
+
+import Foundation
+
+class PostViewController: UIViewController {
+
+ 
+}
+
